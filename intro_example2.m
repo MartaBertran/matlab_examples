1e66b0b5380449b83ea8dbfe757bc8db2db4ac5e
marta.bertran <martabertrangarcia@gmail.com>
2018-11-23 20:55:09 +0100

    Update intro_example2.m

--- intro_example2.m
+++ intro_example2.m
@@ -3,15 +3,12 @@
 %Audio signals are usually composed by several different frequencies.
 
 %Sampling frequency
-fs = 10
+fs = 10;
 %Number of times that a signal has been sampled
 t = 0:1/fs:20;
 %Amplitude of the signal
 y = sin(1.8*2*pi*t)+sin(2.1*2*pi*t);
 
-plot(t,y)
-
-hold on
 
 %Fourier transform provides information about the content of the frequencies in a signal
 yfft = fft(y);
@@ -19,4 +16,12 @@ yfft = fft(y);
 n=numel(y);
 %Frequencies that correspond to values in yfft
 f = 0:fs/n:fs*(n-1)/n;
+
+%Plots
+subplot(2,1,1);
+plot(t,y)
+title('Audio Signal')
+
+subplot(2,1,2);
 plot(f,abs(yfft))
+title('Fourier Transform')
